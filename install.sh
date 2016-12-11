19ef49ff28420d50eca8c4e916cecf621c251e5d
Chris Dell <cjdell@gmail.com>
2016-12-11 11:05:06 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 mkdir /opt/varnish-backend-manager
-cp ../../bin/varnish-backend-manager /opt/varnish-backend-manager
-cp ../start.sh /opt/varnish-backend-manager
-cp ../varnish-restart.sh /opt/varnish-backend-manager
 
-cp varnish-backend-manager.service /etc/systemd/system/
+cp ./varnish-backend-manager /opt/varnish-backend-manager
+cp ./start.sh /opt/varnish-backend-manager
+cp ./varnish-restart.sh /opt/varnish-backend-manager
+
+cp ./varnish-backend-manager.service /etc/systemd/system/
 systemctl enable varnish-backend-manager
